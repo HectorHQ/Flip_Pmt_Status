4a08dfccfd4aa28187e0cbda6225c310bfd1164d
HectorHQ <109182400+HectorHQ@users.noreply.github.com>
2022-08-04 12:58:00 -0500

    Update main_flip.py

--- main_flip.py
+++ main_flip.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from flip_status_class import UpdateOrder_COD_PAID, UpdateOrder_NET_TERMS_PAID, all_admin_orders_accounting_page, payment_method, UpdateOrder_REMITTED, connect_website
+from flip_status_class import UpdateOrder_COD_PAID, UpdateOrder_NET_TERMS_PAID, all_admin_orders_accounting_page, payment_method, UpdateOrder_REMITTED, connect_website,UpdateOrder_PARTIAL_PAID, amount_collected
 import pandas as pd
 
 
@@ -13,11 +13,15 @@ with col1:
     file = st.file_uploader('Upload list of invoices')
     if file is not None:
         df = pd.read_excel(file,engine='openpyxl')
-        list_invoices = df['Invoice'].astype('str').to_list() 
-        st.write(f'{int(len(list_invoices))} Invoices to Flip')
-       
-    pmt_status = st.radio('Select Payment Status',options=['Paid','Remitted'],index=1)
+        df[['Invoice','Payment Status']] = df[['Invoice','Payment Status']].astype('str') 
+        count_invoices = int(df['Invoice'].value_counts())
+        df[['GMV_Collected','TAX_Collected']] = df[['GMV_Collected','TAX_Collected']].astype('float')
+        st.write(f'{count_invoices} Invoices to Flip')
+        
+    pmt_status = st.radio('Select Payment Status',options=['Paid','Remitted','Partial Paid'],index=1)
     if pmt_status == 'Paid':
+        paymt_method = st.radio('Choose Payment Method',options=['CASH','CHECK','EFT'])
+    elif pmt_status == 'Partial Paid':
         paymt_method = st.radio('Choose Payment Method',options=['CASH','CHECK','EFT'])
     
 
@@ -54,7 +58,26 @@ def flip_to_remitted(headers,list_orders):
         st.write(f'Order # {order}{" - "}{" Order Processed "} ')
         UpdateOrder_REMITTED(headers,qb_invoice_data)
 
-    st.success('Done!')    
+    st.success('Done!')
+    
+    
+def flip_to_PARTIAL_PAID(headers,list_orders,GMV_Collected,TAX_Collected,pmt_method):
+
+    for order in list_orders:
+        order_number = order
+        order_data = all_admin_orders_accounting_page(headers,order_number)
+        
+        qb_invoice_data = {
+            "id": order_data['data']['viewer']['allAdminAccountingOrders']['results'][0]['id'],
+        }
+
+        amount_collected(headers,qb_invoice_data,GMV_Collected,TAX_Collected)
+        UpdateOrder_PARTIAL_PAID(headers,qb_invoice_data)
+        payment_method(headers,qb_invoice_data,pmt_method)
+        st.write(f'{order}{"  "}{" Order Processed "} ')
+        
+    st.success('Done!')
+    
         
 st.markdown('---')
 left_col,center_col,right_col = st.columns(3)
@@ -71,9 +94,23 @@ with col2:
             submit_to_paid = st.button('Submit to Paid')
             if submit_to_paid:
                 headers = connect_website(bearer_token)
-                flip_to_paid(headers,list_invoices,paymt_method)
+                flip_to_paid(headers,df['Invoice'],paymt_method)
         except NameError:
             st.write('Error, reach out to admin')
+            
+    elif pmt_status == 'Partial Paid':
+        try:
+            st.title(f'You have selected {pmt_status} Status, and payment method {paymt_method}')
+            st.warning('Be sure all the information is correct before submitting.')
+            submit_to_Partial_paid = st.button('Submit to Partial Paid')
+            if submit_to_Partial_paid:
+                headers = connect_website(bearer_token)
+                flip_to_PARTIAL_PAID(headers,df['Invoice'],df['GMV_Collected'],df['TAX_Collected'],paymt_method)
+        except NameError:
+            st.write('Error, reach out to admin')        
+            
+            
+            
     else:
         try:
             st.markdown(f'## You have selected {pmt_status} Status.')
@@ -81,7 +118,7 @@ with col2:
             submit = st.button('Submit to Remitted')
             if submit:
                 headers = connect_website(bearer_token)
-                flip_to_remitted(headers,list_invoices)
+                flip_to_remitted(headers,df['Invoice'])
         except NameError:
             st.write('Error, reach out to admin')
 
